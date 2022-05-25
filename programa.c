5fd7247db214de83c57d127f7dfaf44eda3f6c24
Escobar Arenas Jorge <80394861+Escobar-klk@users.noreply.github.com>
2022-05-25 13:19:01 +0200

    Update programa.c
    Añado funciones del banner, rehago el menú y lo actualizo para que sea funcional.

--- programa.c
+++ programa.c
@@ -7,21 +7,35 @@
 #define BUFFER_LEN 1024
 
 
+//INTRODUCIMOS UNA ESTRUCTURA QUE ALMACENA DATOS DEL USUARIO
+
 struct jugador {
-	char numero_jugadores[2];
 	char nombre_jugador[50];
-	char contrasena_jugador[6];
-	
+	char contrasena_jugador[50];
+	char pais_jugador[50];
+	char correo_jugador[50];
+	int puntuacionjugador;
 };
+//INTRODUCIMOS LA FUNCION DIFICULTAD
 
-// Main del programa
+void dificultad(){
+	int opcion2;
+	printf("Indique el nivel de dificultad que desea en su partida\n");
+	printf("\n");
+	printf("1.Facil\n2.Medio\n3.Dificil\n");
+	fflush(stdin);
+	scanf("%d", &opcion2);
+}
+
+//INTRODUCIMOS LA FUNCION REGISTRO DE USUARIO
+
+void registro_usuario(){
 	
-int main(){
+}
 
- 	int numero_jugadores, jugador1, jugador2, opcion1, opcion2, nombre_jugador1, nombre_jugador2,contrasena_jugador1,contrasena_jugador2;	
-	int longitud1,numero, contador=0, imprimirMenu, opcion, R, U, S, r, s, u;
- 	jugador1=1;
- 	jugador2=1;
+//INTRODUCIMOS LA FUNCION DEL BANNER PRINCIPAL
+
+void banner(){
 	printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
 	char banner[]={"*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*{ WOMBAT YINCANA }-*-*-*-*"};
 	unsigned short longitud, indice, espacio, repite, resto;
@@ -36,12 +50,12 @@ int main(){
 		{	for (indice=0; indice<=longitud-1-resto; indice++) // seleccion de indices evitando restantes 
 			{
 				printf("%c", banner[indice]); // imprime indice 
-				if (!resto) usleep(100); // demora el proceso 
+				if (!resto) usleep(1000); // demora el proceso 
 			}
 			for(espacio=1; espacio<=longitud-resto-1; espacio++)
 				printf("\b"); // retrocede 
  
-			if (resto) usleep(200); // demora el proceso 
+			if (resto) usleep(2000); // demora el proceso 
 			printf("\b "); // borra el caracter actual 
 		}
  
@@ -52,17 +66,55 @@ int main(){
 	printf("*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*{ WOMBAT YINCANA }-*-*-*-*\n");
 	printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
 	
-	//MENU : REGLAS, REGISTRO
+}
+
+//INTRODUCIMOS LA FUNCIÓN DE UN BANNER FIJO 
+
+void banner_fijo(){
+	printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+	printf("*-*-*-*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*-{ WOMBAT YINCANA }-*-*-*-*-*{ WOMBAT YINCANA }-*-*-*-*-*-*\n");
+	printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
 	
-	// Menú de opciones
-	printf("Imprimir menu de opciones:\n");
-	printf("R - Reglas del juego\nU - Usuario y contrasena para jugar\nS - Salir del programa\n");
+}
+
+// Main del programa
+	
+int main(){
+
+ 	int numero_jugadores, opcion2;	
+	int longitud1,numero, contador=0, imprimirMenu,opcion,salir_del_juego,salir_al_menu, R, U, S;
+	int num_jugadores,i,q,zor;
+	
+	
+	struct jugador PLAYER[4];
+	
+	//Introducimos la funcion que crea el banner
+	banner();
+		
+	
+	//MENU : REGLAS, REGISTRO ,SALIR
+	
+	zor=1;
+	
+	while(zor=1){
+		
+	system("cls");
+	banner_fijo();
+	
+	printf("BIENVENIDO AL MENU PRINCIPAL :\n");
+	printf("Escoge entre las distintas opciones que presenta el videojuego\n");
+	printf(" 1 - Para visualizar las reglas del juego\n 2 - Usuario y contrasena, para a continuacion  jugar\n 3 - Para salir del programa\n");
 	fflush(stdin);
-	scanf("%c", &opcion);
-	   
-	    switch (opcion) {
-		    case 'R' :  // REGLAS
-		        printf("Reglas del juego.\n");
+	scanf("%d", &opcion);
+	system("cls");
+	
+	
+	if(opcion==1){	// REGLAS
+		
+		banner_fijo();
+				printf("\n");
+				printf("\n");
+		    	printf("Reglas del juego.\n");
 		        printf("Para inciar tu partida en WOMBAT YINCANA, debes regristrarte o iniciar sesion si ya tienes una cuenta.\n");
 			    printf("El juego consta de 5 pruebas divididas en 5 preguntas cada una.\n");
 			    printf("A medida que vayas respondiendo correctamente cada pregunta, se iran sumando tus puntos.\n");
@@ -70,63 +122,99 @@ int main(){
 			    printf("Para poder continuar con las siguientes pruebas, debes acumular un total minimo de 10 puntos.\n");
 				printf("\n");
 				printf("MUCHA SUERTE Y QUE COMIENCE EL JUEGO!\n ");
-			    
-		    case 'U' :
-		    	// USUARIO Y CONTRASEÑA
-		    	printf("Introduce el numero de jugadores 1 (solitario) o 2 (1v1):\n");
-				fflush(stdin);
-				scanf("%c", &opcion1);
 				printf("\n");
-		
-				switch(opcion1){
-					case '1':
-						printf("1 jugador. Introduzca su nombre de usuario:\n");
-						fflush(stdin);
-						scanf("%s", &nombre_jugador1);
-					
-						printf("Introduce tu contrasena jugador 1\n");
-	        			fflush(stdin);
-		        		scanf("%c", &contrasena_jugador1);
-		        		printf("\n");
-		        
-		        	break;
-			
-			
-					case '2':
-						printf("Jugador 1. Introduzca su nombre de usuario:\n");
-						fflush(stdin);
-						scanf("%s", &nombre_jugador1);
-						printf("\n");
+				printf("\n");
+				printf("Pulsa cualquier caracter para volver al menu:\n ");
+				printf("\n");
+				printf("\n");
 				
-						printf("Introduce tu contrasena jugador 1\n");
-	         			fflush(stdin);
-						scanf("%c", &contrasena_jugador1);
-						printf("\n");
-				
-						printf("Jugador 2. Introduzca su nombre de usuario:\n");
-						fflush(stdin);
-						scanf("%s", &nombre_jugador2);
-						printf("\n");
-				
-						printf("Introduce tu contrasena jugador 2\n");
-						fflush(stdin);
-						scanf("%c", &contrasena_jugador2);
-						printf("\n");
-						
-				} break;
-
-			case 'S': // SALIR DEL PROGRAMA
-				printf("Salir del juego");
-				printf("Hasta pronto, les esperamos en su proxima partida de WOMBAT");
-				return 0;
-				
+			fflush(stdin);
+			scanf("%d", &salir_al_menu);
+			if(salir_al_menu==1){
+				q=1;	
+				}
+				else{
+					q=1;
+				}
 		}
+			    
+	else if(opcion==2){ // USUARIO Y CONTRASEÑA
 		
-		printf("Indique el nivel de dificultad que desea en su partida\n");
-		printf("\n");
-		printf("1.Facil\n2.Medio\n3.Dificil\n");
-		scanf("%d", &opcion2);
+		
+			banner_fijo();
+		
+		   	printf("Introduce el numero de jugadores, hay posibilidad de que jueguen\n");
+			printf(" hasta 4 personas:\n");
+			fflush(stdin);
+			scanf("%d", &num_jugadores);
+			printf("\n");	
+			q++;
+	    	struct jugador PLAYER[4];
+			i=1;
+			
+			while(i<=num_jugadores){
+				
+			system("cls");
+			banner_fijo();
+				
+			printf("Jugador %d Introduzca su nombre de usuario:\n",i);
+			fflush(stdin);
+			scanf("%s", &PLAYER[i].nombre_jugador);
+			printf("\n");
+								
+			printf("Introduce tu contrasena jugador %d \n",i);
+			fflush(stdin);
+			scanf("%s", &PLAYER[i].contrasena_jugador);
+			printf("\n");
+			
+			printf("Jugador %d introduce tu pais de origen:\n",i);
+			fflush(stdin);
+			scanf("%s", &PLAYER[i].pais_jugador);
+			printf("\n");
+		
+			printf("Introduce tu correo jugador %d\n",i);
+			fflush(stdin);
+			scanf("%s", &PLAYER[i].correo_jugador);
+			printf("\n");
+			system("cls");		//CON ESTO BORRAMOS LO QUE HAY EN LA PANTALLA
+			i++;
+				}
+				zor++;
+				break;
+				
+		    }
+	else if(opcion==3){	// SALIR DEL PROGRAMA
+				
+		banner_fijo();
+		printf("Saliendo del juego...\n");
+		printf("Hasta pronto, les esperamos en su proxima partida de WOMBAT.\n");
+		printf("Teclea 1 para salir del juego.\n");
+		printf("Teclea cualquier caracter para volver al menu del juego.\n");
+		fflush(stdin);
+		scanf("%d", &salir_del_juego);
+		if(salir_del_juego==1){
+			printf("leo la s\n");
+			
+					return 0;
+				}
+		else{
+				zor=1;
+				printf("Volviendo al menu...\n");
+				}
+			}
 	
+	else{
+		zor=1;
+		printf("Introduzca de nuevo su respuesta, no he podido comprender la anterior\n");
+			
+	}
+	
+	}
+	
+	system("cls");
+	banner_fijo();
+	dificultad();
 	
 	return 0;
+	
 }
